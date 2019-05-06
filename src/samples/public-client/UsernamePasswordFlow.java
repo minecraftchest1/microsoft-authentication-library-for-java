259ba25dc460ec0cdd4c12d694d8d6d4e91b9864
minecraftchest1 <42992675+minecraftchest1@users.noreply.github.com>
2019-05-06 14:40:33 -0600

    Change layout and comments
    Change layout for readability. Add comments.

--- src/samples/public-client/UsernamePasswordFlow.java
+++ src/samples/public-client/UsernamePasswordFlow.java
@@ -3,6 +3,7 @@
 //
 // This code is licensed under the MIT License.
 //
+/**
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files(the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
@@ -20,21 +21,30 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+*/
 
+//import microsoft class
 import com.microsoft.aad.msal4j.*;
 
+//import java classes
 import java.net.MalformedURLException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 
-public class UsernamePasswordFlow {
+//start of class
+public class UsernamePasswordFlow
+{
 
-    public static void main(String args[]) throws Exception {
+    //main
+    public static void main(String args[]) throws Exception 
+    {
         getAccessTokenFromUserCredentials();
     }
 
-    private static void getAccessTokenFromUserCredentials() throws Exception {
+    //get acess token
+    private static void getAccessTokenFromUserCredentials() throws Exception
+    {
         PublicClientApplication app = PublicClientApplication.builder(TestData.PUBLIC_CLIENT_ID)
                 .authority(TestData.AUTHORITY_ORGANIZATION)
                 .build();
@@ -47,8 +57,11 @@ public class UsernamePasswordFlow {
 
         CompletableFuture<AuthenticationResult> future = app.acquireToken(parameters);
 
-        future.handle((res, ex) -> {
-            if(ex != null) {
+        future.handle((res, ex) -> 
+         {
+             //print error message
+            if(ex != null) 
+            {
                 System.out.println("Oops! We have an exception - " + ex.getMessage());
                 return "Unknown!";
             }
@@ -56,14 +69,17 @@ public class UsernamePasswordFlow {
             Collection<Account> accounts = app.getAccounts().join();
 
             CompletableFuture<AuthenticationResult> future1;
-            try {
+            try 
+            {
                 future1 = app.acquireTokenSilently
                         (SilentParameters.builder(Collections.singleton(TestData.GRAPH_DEFAULT_SCOPE),
                                 accounts.iterator().next())
                                 .forceRefresh(true)
                                 .build());
 
-            } catch (MalformedURLException e) {
+            } 
+             catch (MalformedURLException e) 
+             {
                 e.printStackTrace();
                 throw new RuntimeException();
             }
